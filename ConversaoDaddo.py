380e0eb59fbed72f9a6cc3e6899b1e9fda86a291
FelipeMarani <felipejesusdasilva02@gmail.com>
2024-06-17 20:00:13 -0300

    first commit

---
+++ ConversaoDaddo.py
@@ -0,0 +1,5 @@
+preco = int(input("Digite um valor: "))
+print(preco)
+
+precoFloat = float(preco)
+print(precoFloat)
---
+++ Var_Const.py
@@ -0,0 +1,4 @@
+name = str(input("Digite seu nome: "))
+year = int(input("Digite sua idade: "))
+
+print("Bem vindo", name, ",", "vi que você tem ", year, "anos meus parabéns!!")
