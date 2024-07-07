ed7be48d71706420486cbb473f63b9ef98e4054e
FelipeMarani <felipejesusdasilva02@gmail.com>
2024-07-07 16:28:02 -0300

    update

--- Tipo_Operadores/OperadoresAritmeticos.py
+++ Tipo_Operadores/OperadoresAritmeticos.py
@@ -1,10 +1,10 @@
 produto_1 = 10
 produto_2 = 20
 #                                             Ordem de Precedêcia
-print("Potênciação =", produto_1 ** produto_2)      #1°
-print("Divisão Interira =", produto_1 // produto_2) #2°
-print("Multiplicação =", produto_1 * produto_2)     #3°
-print("Divisão", produto_1 / produto_2)             #3°
-print("Resto =", produto_1 % produto_2)             #4°
-print("Soma =", produto_1 + produto_2)              #5°      
-print("Subtração =", produto_1 - produto_2)         #5°
+print("Potênciação =", produto_1 ** produto_2)
+print("Divisão Interira =", produto_1 // produto_2)
+print("Multiplicação =", produto_1 * produto_2)
+print("Divisão", produto_1 / produto_2)
+print("Resto =", produto_1 % produto_2)
+print("Soma =", produto_1 + produto_2)
+print("Subtração =", produto_1 - produto_2)
